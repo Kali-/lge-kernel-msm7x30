RonGokhale/codeaurora_kernel

mmc: msm_sdcc: Use nosync variant to disable sdcc irq
There is a possible deadlock between msmsdcc_sdio_al_lpm() and
msmsdcc_irq(). The former acquires host resource and waits for
pending msmsdcc_irq() to be completed which inturn waits for the same
resource to be released. Fix this by using nosync variant while
disabling sdcc irq.

CRs-Fixed: 299342
Change-Id: I5247981065c8b00f3baa10b39c9df3f5aebea1df
Signed-off-by: Sujith Reddy Thumma <sthumma@codeaurora.org>

drivers/mmc/host/msm_sdcc.c
@@ -3819,7 +3819,7 @@ int msmsdcc_sdio_al_lpm(struct mmc_host *mmc, bool enable)
 	if (enable) {
 		if (!host->sdcc_irq_disabled) {
 			writel_relaxed(0, host->base + MMCIMASK0);
-			disable_irq(host->core_irqres->start);
+			disable_irq_nosync(host->core_irqres->start);
 			host->sdcc_irq_disabled = 1;
 		}
 

Kali-/lge-kernel-msm7x30

Change-Id: I8273976f78eb45852c32517294081630f91e38d7

drivers/mmc/host/msm_sdcc.c
@@ -2229,7 +2229,7 @@ int msmsdcc_sdio_al_lpm(struct mmc_host *mmc, bool enable)
 	if (enable) {
 		if (!host->sdcc_irq_disabled) {
 			writel_relaxed(0, host->base + MMCIMASK0);
-			disable_irq(host->irqres->start);
+			disable_irq_nosync(host->irqres->start);
 			host->sdcc_irq_disabled = 1;
 		}
 		if (host->clks_on) {